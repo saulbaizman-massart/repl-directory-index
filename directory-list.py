2c4994a5fe7e707ffef13fe5f0d2142182e6190c
Saul Baizman <saul.baizman@massart.edu>
2020-11-24 13:43:13 -0500

    Made major modifications.
    + Replaced hard-coded name of repl with variable.
    + Restricted links to only html, pdf, and text files.
    + Included sub-subfolders.

--- directory-list.py
+++ directory-list.py
@@ -1,56 +1,85 @@
 #!/opt/virtualenvs/python3/bin/python
 '''
-Saul says, DO NOT DELETE THIS FILE.
+DO NOT DELETE THIS FILE.
 
-Print a directory with links to files.
+This program prints a directory with links to HTML, PDF, and text files.
 
 To create the directory, press Command + Shift + S. In the Terminal window, enter:
 
-$ chmod +x directory-list.py
-$ ./directory-list.py > index.html
+$ python directory-list.py
 
 WARNING: this will over-write index.html.
-
 '''
 
 import os
 
-folders = os.listdir('.')
+def is_valid_file_type (filename ) :
 
-print ('''
+  valid_file_types = [ '.pdf', '.html', '.txt' ]
+
+  _, extension = os.path.splitext(filename)
+
+  if extension.lower() in valid_file_types:
+    return True
+  else:
+    return False
+
+def format_link ( url, target ) :
+  return '<li><a href="/%s" target="_blank">%s</a></li>' % ( url, target )
+
+repl_owner = os.environ['REPL_OWNER']
+repl_name = os.environ['REPL_SLUG'].replace('-',' ')
+
+folders = os.listdir('.')
+'''
+All repls can have a file named ".env" which stores key/value pairs that can be easily read by Python. More details are here:
+https://docs.repl.it/repls/secret-keys
+'''
+header='''
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
-    <title>final project</title>
+    <title>%s: %s</title>
   </head>
   <body>
-    <h1>Final Project</h1>
-''')
+    <h1>%s: %s</h1>
+''' % (repl_owner, repl_name, repl_owner, repl_name )
 
-#print (folders)
+content = []
 
-print('<ul>')
+content.append('<ul>')
 for folder in folders:
   if os.path.isdir(folder): # is it a folder?
-    print('<li>%s</li>' % folder )
-    print('<ul>')
+    content.append('<li>%s</li>' % folder )
+    content.append('<ul>')
     files = os.listdir(folder)
     #print(files)
     for file in files:
       if os.path.isfile(os.path.join(folder,file)): # is it a file?
-        print('<li><a href="/%s" target="_blank">%s</a></li>' % ( os.path.join(folder,file), file ) )
+        # is it a pdf or html file?
+        if is_valid_file_type (file):
+          content.append( format_link ( os.path.join(folder,file), file ) )
       if os.path.isdir(os.path.join(folder,file)):
         subfolder_files = os.listdir(os.path.join(folder,file))
         for subfolder_file in subfolder_files:
-          print('<li><a href="/%s" target="_blank">%s</a></li>' % ( os.path.join(folder,file,subfolder_file), subfolder_file ) )
+        # is it a pdf or html file?
+          if is_valid_file_type (subfolder_file):
+            content.append( format_link ( os.path.join(folder,file,subfolder_file), subfolder_file ) )
     
-    print('</ul>')
+    content.append('</ul>')
   
-print('</ul>')
+content.append('</ul>')
 
-print ('''
+footer='''
   </body>
 </html>
-''')
+'''
+
+# write to the file
+index = open("index.html", "w")
+index.write(header)
+index.write(''.join(content))
+index.write(footer)
+index.close()
