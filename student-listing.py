f78cc4c80fb8d746eeaf02c945509cb51dc2f008
Saul Baizman <saul.baizman@massart.edu>
2020-12-01 21:28:52 -0500

    Reformatted student directory.

--- student-listing.py
+++ student-listing.py
@@ -11,6 +11,8 @@ To update to the latest version:
 $ wget -O student-listing.py https://raw.githubusercontent.com/saulbaizman-massart/repl-directory-index/master/student-listing.py
 '''
 
+import os
+
 def format_link ( url, target ) :
     """Format a link."""
     return '<a href="https://%s" target="_blank" rel="noopener">%s</a>' % ( url, target )
@@ -59,12 +61,15 @@ header='''
     li { line-height: 1.55em }
     a { text-decoration: none; color: red; font-weight: bold }
     a:hover, a:focus { background-color: rgba(250,0,0,.1) }
-    body > div.grid_parent > li { list-style-type: none; padding-left: 0; font-size: 2rem; font-weight: bold }
-    body > div.grid_parent > li > ul { margin-top:0; padding-left: 0 }
-    body > div.grid_parent > li > ul > li { font-size: 1rem; font-weight: normal }
-    body > div.grid_parent > li > ul > li::marker { content: "— " }
-    body > div.grid_parent > li > ul > li > span.separator { color: rgb(180,180,180) }
-    body > div.grid_parent { display: grid; grid-template-columns: repeat(4,1fr); grid-template-rows: repeat(3,1fr); grid-auto-flow: column }
+    body > div.grid_parent > div.student { list-style-type: none; list-style-position: inside ; padding-left: 0; font-size: 2rem; font-weight: bold; background-color: #eee; border-radius: 10px ; padding: 20px 20px 0 35px ; }
+    body > div.grid_parent > div.student > section.links > ul { margin-top:0; padding-left: 0 }
+    body > div.grid_parent > div.student > section.links > ul > li { font-size: 1rem; font-weight: normal }
+    body > div.grid_parent > div.student > section.links > ul > li::marker { content: "— " }
+    body > div.grid_parent > div.student > section.links > ul > li > span.separator { color: rgb(180,180,180) }
+    body > div.grid_parent { display: grid; grid-template-columns: repeat(4,1fr); grid-template-rows: repeat(3,1fr); grid-auto-flow: column ; grid-gap: 30px }
+    img.avatar { width: 128px; height: 128px; background-color: #888 ; border-radius: 50%%; box-shadow: 0 0 6px 3px #ccc }
+    p.avatar_container { text-align: center; margin: 10px 0 }
+    p.repl_user_link_container { text-align: center; margin: 10px 0 }
     </style>
   </head>
   <body>
@@ -76,6 +81,8 @@ footer='''
 </html>
 '''
 
+image_subfolder = 'img'
+
 content = []
 
 content.append('<div class="grid_parent">')
@@ -84,16 +91,23 @@ content.append('<div class="grid_parent">')
 for student in sorted (name_map.keys()):
     real_name = student.lower()
     repl_username = name_map[student]
-    content.append ( '<li>%s' % format_link ( 'repl.it/@%s' % repl_username, real_name ) )
+    
+    image = '%s/%s.png' % ( image_subfolder, real_name )
+    if not os.path.exists(image):
+        image = '%s/blank.png' % image_subfolder
+    content.append ( '<div class="student"><p class="avatar_container"><img class="avatar" src="%s" title="%s"></p>' % ( image, real_name ) )
+    content.append ( '<section class="links">' )
+    content.append ( '<p class="repl_user_link_container">%s</p>' % format_link ( 'repl.it/@%s' % repl_username, real_name ) )
     content.append('<ul>')
 
     # loop through each student's list of repls
     for repl in repl_map[repl_username]:
         repl_clean = repl.lower().replace('-',' ')
-        content.append ( '<li>%s<br>%s <span class="separator">&bull;</span> %s</li>' % ( repl_clean, format_link('repl.it/@%s/%s/' % ( repl_username, repl ) , 'repl'), format_link('%s.%s.repl.co' % ( repl, repl_username ), 'website') ) )
+        content.append ( '<li>%s: %s <span class="separator">&bull;</span> %s</li>' % ( repl_clean, format_link('repl.it/@%s/%s/' % ( repl_username, repl ) , 'repl'), format_link('%s.%s.repl.co' % ( repl, repl_username ), 'website') ) )
 
     content.append('</ul>')
-    content.append('</li>')
+    content.append( '</section>' )
+    content.append('</div>')
 
 content.append('</div>')
 
