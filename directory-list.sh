97d3eebb134a848d20f56b60d0dbf8200fdd1297
Saul Baizman <saul.baizman@massart.edu>
2020-12-07 18:46:27 -0500

    Cleaned up script syntax.
    Added variables.

--- directory-list.sh
+++ directory-list.sh
@@ -6,32 +6,35 @@
 #
 
 SELF=${0##*/}
-WGET=`which wget`
-PYTHON=`which python`
-RM=`which rm`
-PROGRAM=directory-list.py
-URL=https://raw.githubusercontent.com/saulbaizman-massart/repl-directory-index/master/${PROGRAM}
+WGET=$(which wget)
+PYTHON=$(which python)
+RM=$(which rm)
+DIRECTORY_LIST=directory-list.py
+GITHUB_USER=saulbaizman-massart
+GITHUB_REPO=repl-directory-index
+GITHUB_BRANCH=master
+DOWNLOAD_URL=https://raw.githubusercontent.com/${GITHUB_USER}/${GITHUB_REPO}/${GITHUB_BRANCH}/${DIRECTORY_LIST}
 
 echo
-echo Starting $SELF...
+echo Starting ${SELF}...
 
 # download latest version of program
 echo
-echo Downloading latest version of ${PROGRAM} from ${URL}...
-echo ${WGET} -O ${PROGRAM} ${URL}
-${WGET} -O ${PROGRAM} ${URL}
+echo Downloading latest version of ${DIRECTORY_LIST} from ${DOWNLOAD_URL}...
+echo ${WGET} -O ${DIRECTORY_LIST} ${DOWNLOAD_URL}
+${WGET} -O ${DIRECTORY_LIST} ${DOWNLOAD_URL}
 
 # run python program
 echo
-echo Running ${PROGRAM} to generate repl toc...
-echo ${PYTHON} ${PROGRAM}
-${PYTHON} ${PROGRAM}
+echo Running ${DIRECTORY_LIST} to generate repl toc...
+echo ${PYTHON} ${DIRECTORY_LIST}
+${PYTHON} ${DIRECTORY_LIST}
 
 # delete python program
 echo
-echo Deleting ${PROGRAM}... 
-echo ${RM} ${PROGRAM}
-${RM} ${PROGRAM}
+echo Deleting ${DIRECTORY_LIST}... 
+echo ${RM} -f ${DIRECTORY_LIST}
+${RM} -f ${DIRECTORY_LIST}
 
 echo
 echo ...done.
